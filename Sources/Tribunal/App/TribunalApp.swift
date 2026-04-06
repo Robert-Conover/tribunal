afd3ae2c325262018ec8b8ae1cf79f72c1bd8557
Robert-Conover <robert@conov.io>
2026-04-06 19:34:35 -0400

    Restore keyboard focus and paste support in text input fields

--- Sources/Tribunal/App/TribunalApp.swift
+++ Sources/Tribunal/App/TribunalApp.swift
@@ -1,9 +1,19 @@
 import SwiftUI
 import SwiftData
+import AppKit
 
 @main
 struct TribunalApp: App {
+    @NSApplicationDelegateAdaptor(TribunalAppDelegate.self) private var appDelegate
     @StateObject private var settings = UserSettings()
+    
+    init() {
+        // SwiftUI apps launched via `swift run` can start without normal app activation,
+        // which prevents text inputs from receiving keyboard focus.
+        DispatchQueue.main.async {
+            ensureTribunalAppIsActive()
+        }
+    }
 
     var body: some Scene {
         WindowGroup {
@@ -13,6 +23,9 @@ struct TribunalApp: App {
                 )
             )
             .environmentObject(settings)
+            .onAppear {
+                ensureTribunalAppIsActive()
+            }
         }
         .modelContainer(for: Case.self)
         .defaultSize(width: 1100, height: 750)
@@ -25,6 +38,18 @@ struct TribunalApp: App {
             }
         }
     }
+}
+
+private final class TribunalAppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        ensureTribunalAppIsActive()
+    }
+}
+
+private func ensureTribunalAppIsActive() {
+    NSApp.setActivationPolicy(.regular)
+    NSApp.activate(ignoringOtherApps: true)
+    NSApp.windows.first?.makeKeyAndOrderFront(nil)
 }
 
 extension Notification.Name {
